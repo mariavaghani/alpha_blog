1a467caa7ea2b1e47b1fec0899ac8ebee612da0b
mariavaghani <maria.vaghani@gmail.com>
2021-06-17 10:59:22 -0400

    Display error and success messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,20 @@ class ArticlesController < ApplicationController
   def index
     @article_list = Article.all
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description,
+      :genre))
+    if @article.save
+      flash[:notice] = "Article was created successfully"
+      redirect_to @article
+    else
+      render 'new'
+
+    end
+  end
 end
