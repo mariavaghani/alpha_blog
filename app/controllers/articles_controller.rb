aa3d343746ea51433fba44e45ffad697414cc5f0
mariavaghani <maria.vaghani@gmail.com>
2021-06-17 12:35:31 -0400

    add edit and update actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,7 +19,23 @@ class ArticlesController < ApplicationController
       redirect_to @article
     else
       render 'new'
+    end
+  end
 
+  def edit
+
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    
+    if @article.update(params.require(:article).permit(:title, :description,
+      :genre))
+      flash[:notice] = "Article was updated successfully"
+      redirect_to @article
+    else
+      render 'edit'
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index, :new, :create]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  # For details on the DSL available within this file, see
+  # https://guides.rubyonrails.org/routing.html
 end
