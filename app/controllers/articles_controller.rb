dbc64319d547ebfd80ae201ef86ada3995e36efd
mariavaghani <maria.vaghani@gmail.com>
2021-06-17 16:05:01 -0400

    add partials for errors_in_form, _form, and _messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,9 @@
 class ArticlesController < ApplicationController
+
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def show
-    @article = Article.find(params[:id])
+
   end
 
   def index
@@ -12,8 +15,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description,
-      :genre))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was created successfully"
       redirect_to @article
@@ -24,14 +26,11 @@ class ArticlesController < ApplicationController
 
   def edit
 
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
 
-    if @article.update(params.require(:article).permit(:title, :description,
-      :genre))
+    if @article.update(article_params)
       flash[:notice] = "Article was updated successfully"
       redirect_to @article
     else
@@ -40,8 +39,16 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
+  end
+
+  private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description, :genre)
   end
 end
