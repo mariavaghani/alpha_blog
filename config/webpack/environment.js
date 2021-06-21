42f118be27a4612c92056a484bb55cbc9d378f28
mariavaghani <maria.vaghani@gmail.com>
2021-06-21 09:33:57 -0400

    add Bootstap css files and override navbar appearance

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require("webpack")
+
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+  $: 'jquery',
+  JQuery: 'jquery',
+  Popper: ['popper.js', 'default']
+}))
+
 module.exports = environment
