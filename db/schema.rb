02d88face5552668b96241b803cba62fe5ba026c
mariavaghani <maria.vaghani@gmail.com>
2021-06-24 19:08:52 -0400

    add user_id to the model and assign all articles to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_24_124622) do
+ActiveRecord::Schema.define(version: 2021_06_24_224148) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_06_24_124622) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "genre"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
