693937c758507c8e01a5680829b745d0980bba5e
mariavaghani <maria.vaghani@gmail.com>
2021-06-25 22:13:27 -0400

    add user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_action :set_user, only: [:edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def new
     @page_name = 'Sign up for Alpha BLog'
@@ -31,6 +31,9 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @article_list = @user.articles
+  end
 
   private
   def set_user
