7be846db4601e5b4d6794d722c17bc2f4a724e65
mariavaghani <maria.vaghani@gmail.com>
2021-06-27 00:51:40 -0400

    create login and logout actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,21 @@ class SessionsController < ApplicationController
   end
 
   def create
-    byebug
+    # byebug
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Hi, #{user.username}! Welcome back!"
+      redirect_to user
+    else
+      flash.now[:alert] = "Username and password combination does not match our records"
+      render "new"
+    end
   end
 
   def destroy
-
+    session[:user_id] = nil
+    flash[:notice] = "Logged out"
+    redirect_to root_path
   end
 end
